b5eca265056fb2e677480d3b4af947b4ee763ce6
wendymasese <wendy.masese@student.moringaschool.com>
2024-06-06 17:11:42 +0300

    complete lab

---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react"
+import UserCard from "../components/UserCard";
+import NavBar from "../components/NavBar";
+
+function Home() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() =>{
+    fetch("http://localhost:4000/users")
+      .then(r => r.json())
+      .then(data => setUsers(data))
+      .catch(error => console.error(error))
+  }, [])
+
+  const userList = users.map(user =>{
+    return <UserCard key={user.id} user={user}/>
+  });
+
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <h1>Home!</h1>
+        {userList}
+      </main>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/components/Profile.js
@@ -0,0 +1,16 @@
+import NavBar from "../components/NavBar";
+
+function UserProfile() {
+  return(
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <h1>User Profile</h1>
+      </main>
+    </>
+  );
+};
+
+export default UserProfile;
